fd11fb8ff41e1e1b3fd506fe3ad6126da6ff2484
Rohan Desai <159284017+rohan13122002@users.noreply.github.com>
2025-11-26 16:26:44 +0530

    Add files via upload

---
+++ WiproDay2/Exercise 4.java
@@ -0,0 +1,13 @@
+//4. Equal Character in String
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        //write your answer here
+        Scanner sc = new Scanner();
+        String s = sc.nextLine();
+    }
+
+}
