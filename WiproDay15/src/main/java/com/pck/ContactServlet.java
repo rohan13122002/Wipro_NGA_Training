d6512c72bbc73b07f2511a8361a100259fbacd0f
Rohan Desai <159284017+rohan13122002@users.noreply.github.com>
2025-11-26 14:06:41 +0530

    Add files via upload

---
+++ WiproDay15/src/main/java/com/pck/ContactServlet.java
@@ -0,0 +1,53 @@
+package com.pck;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ContactServlet
+ */
+@WebServlet("/ContactServlet")
+public class ContactServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+	private List<Contact> contacts = new ArrayList<>();
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String phone = request.getParameter("phone");
+
+        contacts.add(new Contact(name, email, phone));
+
+        request.setAttribute("contacts", contacts);
+        request.getRequestDispatcher("viewContacts.jsp").forward(request, response);
+    }
+    public ContactServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
+
+}
