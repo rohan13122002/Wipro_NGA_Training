d4cd8f7a9f0df32d74def5cf0134c4601834a840
Rohan Desai <159284017+rohan13122002@users.noreply.github.com>
2025-12-03 23:28:23 +0530

    Add files via upload

---
+++ Web-Framework & ORM, Spring & Springboot/SpringSecurityJDBC/src/main/java/com/example/dto/UserRequestDTO.java
@@ -0,0 +1,14 @@
+package com.example.dto;
+
+import lombok.Data;
+
+//when request will be send consist of json data it will interact with DTO first 
+@Data
+public class UserRequestDTO {
+	
+
+	private String username;
+	private String password;
+	private String role;
+
+}
---
+++ Web-Framework & ORM, Spring & Springboot/ValidateExample/src/main/java/com/example/ValidExampleApp.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidExampleApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidExampleApp.class, args);
+	}
+
+}
